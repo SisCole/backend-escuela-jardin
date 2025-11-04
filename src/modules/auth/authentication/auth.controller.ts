d7d00da878b1a58e7692709a5affeec68854aa67
Jose Luis <joseluisjlgd123@gmail.com>
2025-11-03 19:09:36 -0500

    Fixed: Add maxage and sameSite

--- src/modules/auth/authentication/auth.controller.ts
+++ src/modules/auth/authentication/auth.controller.ts
@@ -5,13 +5,21 @@ import { AuthRequest } from './types/auth.type';
 export async function login(req: AuthRequest, res: Response) {
     const { email, password } = req.body;
     const auth = await AuthService.loginService(email, password);
-    res.cookie('token', auth.token, { httpOnly: true, secure: process.env.NODE_ENV === 'production' });
+    res.cookie('token', auth.token, { 
+        httpOnly: true, 
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: 'strict',
+        maxAge: 60 * 60 * 24 , // 24 horas
+    });
     res.status(200).json({ message: "Login exitoso" });
 }
 
-
 export async function logout(_: Request, res: Response) {
-    res.clearCookie('token');
+    res.clearCookie('token', {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: 'strict',
+    });
     res.status(200).json({ message: "Logout exitoso" });
 }
 
